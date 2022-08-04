c67f43270e94ad0e4fba75adfd0cdddb2007c136
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2022-08-04 20:56:46 +0700

    Update CSES - Binary Subsequences.cpp

--- CSES - Binary Subsequences.cpp
+++ CSES - Binary Subsequences.cpp
@@ -59,11 +59,6 @@ string check(int a, int b){
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    #define Task "A"
-    if (fopen(Task".inp", "r")){
-        freopen(Task".inp", "r", stdin);
-        freopen(Task".out", "w", stdout);
-    }
 
     cin >> n;
     fin.resize(n+1);
